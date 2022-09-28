98ede2b41b53f84f6225d13499af8a739c23d7f9
tami <xorud7979@gmail.com>
2022-09-28 14:54:34 +0900

    feat: react-hook-form 추가

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,10 +5,15 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Modal, Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
+
 
 const rows = [
   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
@@ -19,20 +23,40 @@ const rows = [
   createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
-export default function BasicTable() {
+export default function ContactList() {
+
+  const {register} = useForm();
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "none",
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <h2>직원 비상 연락망</h2>
+        <Table>
           <TableHead>
             <TableRow>
               <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-              <TableCell align="right">수정하기</TableCell>
-              <TableCell align="right">삭제하기</TableCell>
+              <TableCell>Calories</TableCell>
+              <TableCell>Fat&nbsp;(g)</TableCell>
+              <TableCell>Carbs&nbsp;(g)</TableCell>
+              <TableCell>Protein&nbsp;(g)</TableCell>
+              <TableCell>수정하기</TableCell>
+              <TableCell>삭제하기</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -48,8 +72,86 @@ export default function BasicTable() {
                 <TableCell align="right">{row.fat}</TableCell>
                 <TableCell align="right">{row.carbs}</TableCell>
                 <TableCell align="right">{row.protein}</TableCell>
-                <TableCell align="right">버튼</TableCell>
-                <TableCell align="right">버튼</TableCell>
+                <TableCell>
+
+                  {/* 수정하기 모달창 */}
+                  <button
+                    className="edit"
+                    onClick={handleOpen}
+                    style={{ width: 35, margin: 15 }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                      />
+                    </svg>
+                  </button>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        <form>
+                          <h4>직원 비상 연락망 수정</h4>
+                          <label>직위·직책</label>
+                          <input {...register("role", { required: true })} /> <br/>
+                          <label>성명</label>
+                          <input {...register("name", { required: true })} /><br/>
+                          <label>전화번호</label>
+                          <input {...register("tel", { required: true })} /><br/>
+                          <label>이메일</label>
+                          <input {...register("email", { required: true })} /><br/>
+                          <label>생년월일</label>
+                          <input {...register("birth", { required: true })} />
+                        </form>
+                      </Typography>
+                      <button onClick={handleClose}>닫기</button>
+                      <button onClick={handleClose}>수정하기</button>
+                    </Box>
+                  </Modal>
+                </TableCell>
+
+                {/* 삭제하기 모달창 */}
+                <TableCell>
+                  <button
+                    className="delete"
+                    style={{ width: 35, margin: 15 }}
+                    onClick={() => {
+                      delStudents(student.id);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -61,9 +163,19 @@ export default function BasicTable() {
     width: 1000px;
     margin: 50px auto;
   }
+  h2 {
+    text-align: center;
+  }
+  thead {
+    background: #D2DAFF;
+  }
+  button {
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
   `}
       </style>
     </>
   );
 }
-
